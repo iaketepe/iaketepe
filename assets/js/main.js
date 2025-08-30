c2f32334d5526a21866c808a098ab520f8a909cd
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-29 22:12:28 -0400

    Removed wrapper styling that was being overwritten by spotlight.stylex to improve maintainability and coherence

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Play initial animations on page load
-    window.addEventListener('load', () => {
-        setTimeout(() => {
-            document.body.classList.remove('is-preload');
-        }, 100);
-    });
+    const container = document.getElementById("lazy-loading");
+    const skeleton = document.getElementById("skeleton");
+    const AMT = document.getElementById("AMT");
 
+
+
+    const AboutMeText = ["I'm a systems-based thinker with experience across information technology, data science, and enterprise architecture. Whether I'm streamlining workflows, writing documentation, or designing software and data/information systems to support future objectives, I adapt to different roles and challenges to keep things running smoothly.",
+        "Currently, I'm focused on deepening my understanding of how software systems communicate and handle data—working with backend services, APIs, and data pipelines—to build effective, connected solutions.",
+        "Outside of work, I enjoy reading, discovering new music, and diving into stories—whether they come from books, people, or data."];
+
+    AboutMeText.forEach(text => {
+        const p = document.createElement("p");
+        p.textContent = text;
+        AMT.appendChild(p);
+      });
+
+
+    skeleton.remove();
+    AMT.classList.remove("hidden");
 });
