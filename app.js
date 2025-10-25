eca8a30ce7d9ede0b2f1f496c03034b2659f139f
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-10-25 16:30:41 -0400

    Removed icons.js. Updated CSP

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((_, res, next) => {
   const nonce = res.locals.nonce;
   res.setHeader(
     "Content-Security-Policy",
-    `default-src 'self'; script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://challenges.cloudflare.com; frame-src https://challenges.cloudflare.com; img-src 'self' data:; style-src 'self' 'nonce-${nonce}';`
+    `default-src 'self'; base-uri 'self'; script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://challenges.cloudflare.com 'unsafe-eval'; frame-src https://challenges.cloudflare.com; img-src 'self' data:; style-src 'self' 'nonce-${nonce}';`
   );
   next();
 });
